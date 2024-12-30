5a484f882422743456ecd832867ed3ef41ae4405
ocsoriram <ossoriram@gmail.com>
2024-12-30 12:32:04 +0900

    prismaでdatabaseを準備した

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '1mb', // リクエストボディのサイズ制限
+      allowedOrigins: ['http://localhost:3000'], // 許可されるオリジン
+    },
+  },
 };
 
 export default nextConfig;
