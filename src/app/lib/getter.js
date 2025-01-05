be07958a772a7dc1448cc7106dba42b512e45404
ocsoriram <ossoriram@gmail.com>
2025-01-05 23:52:55 +0900

    getAllReviews関数を定義

---
+++ src/app/lib/getter.js
@@ -0,0 +1,12 @@
+import prisma from "./prisma";
+
+
+
+export async function getAllReviews() {
+  //読了日降順で取得
+  return await prisma.reviews.findMany({
+    orderBy: {
+      read: 'desc'
+    }
+  });
+}
