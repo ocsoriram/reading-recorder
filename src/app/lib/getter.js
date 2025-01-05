666f853da079eeb3d429634ad0b6f0997ba1c3d6
ocsoriram <ossoriram@gmail.com>
2025-01-06 00:23:44 +0900

    grtAllReviews()にtry-catchを追加

--- src/app/lib/getter.js
+++ src/app/lib/getter.js
@@ -4,9 +4,13 @@ import prisma from "./prisma";
 
 export async function getAllReviews() {
   //読了日降順で取得
-  return await prisma.reviews.findMany({
-    orderBy: {
-      read: 'desc'
-    }
-  });
+  try {
+    const reviews = await prisma.reviews.findMany({
+      orderBy: { read: 'desc' },
+    });
+    return reviews;
+  } catch (error) {
+    console.error("Prisma Error:", error);
+    throw new Error("Failed to fetch reviews");
+  }
 }
