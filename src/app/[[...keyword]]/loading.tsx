a7b01f5c98f945c13a8792344374f49dfcabdecf
ocsoriram <ossoriram@gmail.com>
2025-01-06 16:01:31 +0900

    loading.tsxのタイポを修正

--- src/app/[[...keyword]]/loading.tsx
+++ src/app/[[...keyword]]/loading.tsx
@@ -1,7 +1,7 @@
 export default function ApiLoading() {
   return (
     <div className="flex justify-center" aria-label="Now loading...">
-      <div className="animate-spin h-20 w-20 mt-5 boader-8 boader-blue-500 rounded-full border-b-transparent"></div>
+      <div className="animate-spin h-20 w-20 mt-5 border-8 border-blue-500 rounded-full border-b-transparent"></div>
     </div>
   )
 }
