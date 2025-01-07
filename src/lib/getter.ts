89d6a3cbc6fd38d96f3a7172c7627fbbc77f66b5
ocsoriram <ossoriram@gmail.com>
2025-01-08 02:08:18 +0900

    追跡から外れていたファイルをgitに追加(DB関係)

--- src/lib/getter.ts
+++ src/lib/getter.ts
@@ -1,32 +1,32 @@
-import { Volume } from "../../types/api/googlebooks";
-import prisma from "./prisma";
+import { Volume } from '../types/api/googlebooks';
+import prisma from './prisma';
 
 export async function getAllReviews() {
   //読了日降順で取得
   try {
     const reviews = await prisma.reviews.findMany({
-      orderBy: { read: "desc" },
+      orderBy: { read: 'desc' },
     });
     return reviews;
   } catch (error) {
-    console.error("Prisma Error:", error);
-    throw new Error("Failed to fetch reviews");
+    console.error('Prisma Error:', error);
+    throw new Error('Failed to fetch reviews');
   }
 }
 
 //API経由で取得した書籍情報から必要な情報だけをオブジェクトに詰め替える
 export function createBook(book: Volume) {
-  const authors = book.volumeInfo.authors;
+  const authors = book.volumeInfo?.authors || [];
   const price = book.saleInfo?.listPrice;
-  const img = book.volumeInfo.imageLinks;
+  const img = book.volumeInfo?.imageLinks;
   return {
     id: book.id,
     title: book.volumeInfo.title,
-    author: authors ? authors.join(",") : "",
+    author: authors ? authors.join(',') : '',
     price: price ? price.amount : 0,
     publisher: book.volumeInfo.publisher,
     published: book.volumeInfo.publishedDate,
-    image: img ? img.smallThumbnail : "/vercel.svg",
+    image: img ? img.smallThumbnail : '/vercel.svg',
   };
 }
 
