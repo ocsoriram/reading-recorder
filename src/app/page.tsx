210e51e851afd1827fefdd5e9df0472a75b20393
ocsoriram <ossoriram@gmail.com>
2025-01-08 02:09:26 +0900

    BookDetailProps型定義を外部化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import LinkedBookDetails from '../components/LinkedBookDetails';
-import { getAllReviews } from './lib/getter';
+import { getAllReviews } from '../lib/getter';
 
 //ルートセグメントコンフィグ。レイアウト/ページ単位の設定。ページを動的に処理するかを表す情報。
 //これにより、レイアウト/ページの個別の設定施せる。
-export const dynamic = 'force-dynamic'; 
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
-
   const reviews = await getAllReviews();
   console.log(reviews);
   return (
     <>
-      {/*取得したレビュー情報を元にリストを作成 */}    
+      {/*取得したレビュー情報を元にリストを作成 */}
       {reviews.map((b, i) => (
         <LinkedBookDetails book={b} index={i + 1} key={b.id} />
       ))}
