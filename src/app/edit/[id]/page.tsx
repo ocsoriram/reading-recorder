39046627a49f0cacd2b9e46f1e638c9e16f0f69e
ocsoriram <ossoriram@gmail.com>
2025-01-08 02:10:39 +0900

    BookDatailsPrors型定義の外部化によりバグが修正された

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,22 +1,25 @@
-import BookDetails from "../../../components/BookDetails";
-import { getBookById, getReviewById } from "../../lib/getter";
+import BookDetails from '../../../components/BookDetails';
+import FormEdit from '../../../components/FormEdit';
+import { getBookById, getReviewById } from '../../../lib/getter';
 
-
-
-export default async function EditPage({ params }) {
+export default async function EditPage({ params }: { params: { id: string } }) {
   const book = await getBookById(params.id);
   const review = await getReviewById(params.id);
   //YYYY-MM-DD 形式の日付を生成
   const read = (review?.read || new Date()).toLocaleDateString('sv-SE');
-  
-  return(
-    <div id="form">
-      {/* FIX ME 不必要なproperty(memo,read)を要求している。*/}
+
+  return (
+    <div id='form'>
       <BookDetails book={book} />
       <hr />
       {/*編集フォームを生成　*/}
-      <FormEdit src={{id: book.id, read, memo: review?.memo }} />
+      <FormEdit
+        src={{
+          id: book.id,
+          read,
+          memo: review?.memo || '',
+        }}
+      />
     </div>
-  )
-  
+  );
 }
