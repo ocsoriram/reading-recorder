c4f89d53dd6bcb8cedbefbeffd347c92394f2bcf
ocsoriram <ossoriram@gmail.com>
2025-01-14 01:16:53 +0900

    enterで検索ができるように機能実装

--- src/app/books/[[...keyword]]/layout.tsx
+++ src/app/books/[[...keyword]]/layout.tsx
@@ -12,23 +12,25 @@ export default function BooksLayout({ children }: LayoutProps) {
   const router = useRouter();
   const txtKeyword = useRef<HTMLInputElement>(null);
   //[検索]ボタンクリック時に「books/keyword」へリダイレクト
-  const handleSearch = () => {
-    if (txtKeyword.current) {
-      router.push(`/books/${txtKeyword.current.value}`);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const keyword = txtKeyword.current?.value;
+    if (keyword) {
+      console.log('Navigating to:', `/books/${keyword}`); // デバッグログ
+      router.push(`/books/${keyword}`);
     }
   };
 
   return (
     <>
-      <form className='mt-2 mb-4'>
+      <form className='mt-2 mb-4' onSubmit={handleSubmit}>
         <input
           type='text'
           ref={txtKeyword}
-          className='bg-gray-1000 text-black border border-gray-600 rounded mr-2 px-2 py-2 focus:bg-white focus:outline-none focus:border-red-500'
+          className='bg-gray-100 text-black border border-gray-600 rounded mr-2 px-2 py-2 focus:bg-white focus:outline-none focus:border-red-500'
         />
         <button
-          type='button'
-          onClick={handleSearch}
+          type='submit'
           className='bg-blue-600 text-white rounded px-4 py-2 hover:bg-blue-500'
         >
           検索
