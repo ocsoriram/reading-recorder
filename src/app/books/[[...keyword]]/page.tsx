7befceefa69df64a26a9c1efeea1a699dae50840
ocsoriram <ossoriram@gmail.com>
2025-01-14 18:19:16 +0900

    renameなどのリファクタリング

--- src/app/books/[[...keyword]]/page.tsx
+++ src/app/books/[[...keyword]]/page.tsx
@@ -1,11 +1,14 @@
 import LinkedBookDetails from '../../../components/LinkedBookDetails';
 import { getBooksByKeyword } from '../../../lib/getter';
 
-type SearchParams = { keyword?: string[] };
+type BookRouteParams = { keyword?: string[] };
 
-export default async function BookResult({ params }: { params: SearchParams }) {
-  const searchParams = await params;
-  const keyword = searchParams.keyword?.[0] || 'React';
+export default async function BookResult({
+  params,
+}: {
+  params: BookRouteParams;
+}) {
+  const keyword = params.keyword?.[0] || 'React';
 
   const books = await getBooksByKeyword(keyword);
 
